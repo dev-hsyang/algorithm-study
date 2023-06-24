58473cca1f21b5a95d8050f11332e2180a78b337
hyunseung yang <dev.hsyang@gmail.com>
2023-06-25 00:35:28 +0900

    fix: 코드트리-두개의사탕

--- src/samsung_sw/codetree/두개의사탕.java
+++ src/samsung_sw/codetree/두개의사탕.java
@@ -78,13 +78,30 @@ public class 두개의사탕 {
 
 	// MAP 을 한 방향에 대해 완전히 기울이는 동작
 	public static void move(int dir) {
-		if(redFirst(dir)) {
+		if(redFirst(dir)) { // 이동을 할 때 빨간사탕이 먼저 움직여야 하는 경우
 			toEnd(RED, dir);
 			toEnd(BLUE, dir);
-		}else {
+		}else { // 파란사탕이 먼저 움직여야하거나, 상관없는 경우
 			toEnd(BLUE,dir);
 			toEnd(RED, dir);
 		}
+	}
+	
+	/**
+	 * 핵심 아이디어 (사탕이 겹칠경우 처리를 위한)
+	 * 좌우로 굴릴때 사탕이 같은 행에 있을경우, 혹은 상하로 굴릴때 사탕이 같은 열에 있을 경우
+	 * 이동을 완료시키면 사탕은 겹치게 된다
+	 * 이에 따라 사탕이 겹쳤을 때 좌표를 처리하기 위해 빨간사탕 파란사탕 중 먼저 움직일 사탕을 찾는다
+	 */
+	public static boolean redFirst(int dir) {
+		if(dir==0) 
+			return (RED.x==BLUE.x && RED.y>BLUE.y); // 우로 움직일 때 두 사탕이 같은 행에 위치하고, 빨간 사탕이 더 오른쪽에 있으면 빨간사탕 먼저 이동
+		else if(dir==1) 
+			return (RED.y==BLUE.y && RED.x>BLUE.x); // 하로 움직일 때 두 사탕이 같은 열에 위치하고, 빨간 사탕이 더 밑에 있으면 빨간사탕 먼저 이동
+		else if(dir==2) 
+			return (RED.x==BLUE.x && RED.y<BLUE.y);
+		else 
+			return (RED.y==BLUE.y && RED.x<BLUE.x);
 	}
 	
 	// 주어진 방향으로 사탕을 끝까지 굴리는 동작
@@ -92,39 +109,20 @@ public class 두개의사탕 {
 		while(true) {
 			int nx = candy.x + DX[dir];
 			int ny = candy.y + DY[dir];
-			if(isCandy(nx, ny))
+			if(isCandy(nx, ny)) // 다음칸에 사탕이 있으면 이동 중지
 				break;
-			if(!isWall(nx, ny)) {
+			if(!isWall(nx, ny)) { // 다음칸이 이동 가능할 때 이동
 				candy.x = nx;
 				candy.y = ny;
 			}
-			if(isExit(candy) || isWall(nx, ny)) 
+			if(isExit(candy) || isWall(nx, ny)) // 이동한 칸이 구멍이거나, 다음칸이 벽일 경우 이동 중지
 				break;
 		}
 	}
 	
-	// 기울일 때 빨간색 공이 반드시 먼저 움직여야할 때를 처리
-	public static boolean redFirst(int dir) {
-		if(dir==0) 
-			return (RED.x==BLUE.x && RED.y>BLUE.y);
-		else if(dir==1) 
-			return (RED.y==BLUE.y && RED.x>BLUE.x);
-		else if(dir==2) 
-			return (RED.x==BLUE.x && RED.y<BLUE.y);
-		else 
-			return (RED.y==BLUE.y && RED.x<BLUE.x);
-	}
-	
-	// 사탕이 구멍에 빠져있는지 확인하는 함수
-	public static boolean isExit(Candy candy) {
-		if(MAP[candy.x][candy.y].equals("O"))
-			return true;
-		return false;
-	}
-	
 	// 이동하려는 칸에 사탕이 있는지 확인하는 함수
 	public static boolean isCandy(int x, int y) {
-		if(x==BLUE.x && y==BLUE.y && !MAP[x][y].equals("O")) // ************************************* 틀린이유: 사탕이 구멍에 빠지고 났을때, 그 칸에 사탕이 겹치지 않는 처리를 하지 않았었다.
+		if(x==BLUE.x && y==BLUE.y && !MAP[x][y].equals("O")) // ************************************* 틀린이유: 사탕이 구멍에 빠지고 났을때, 그 칸에 더이상 사탕이 없는 처리를 하지 않았었다.
 			return true;
 		if(x==RED.x && y==RED.y && !MAP[x][y].equals("O"))
 			return true;
@@ -136,6 +134,13 @@ public class 두개의사탕 {
 		if(x<0 || x>=N || y<0 || y>=M)
 			return true;
 		if(MAP[x][y].equals("#"))
+			return true;
+		return false;
+	}
+	
+	// 사탕이 구멍에 빠져있는지 확인하는 함수
+	public static boolean isExit(Candy candy) {
+		if(MAP[candy.x][candy.y].equals("O"))
 			return true;
 		return false;
 	}
