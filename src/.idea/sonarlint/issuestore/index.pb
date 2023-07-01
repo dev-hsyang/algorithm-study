b4961ab58d683448cb0785dc58d08a1bf4fbdcc2
hyunseung yang <dev.hsyang@gmail.com>
2023-07-01 16:15:40 +0900

    add: 백준-숨바꼭질 (완)

--- src/.idea/sonarlint/issuestore/index.pb
+++ src/.idea/sonarlint/issuestore/index.pb
@@ -9,8 +9,6 @@ P
  baekjoon/미세먼지안녕.java,d/2/d203b4d7a45ed22888d53fb5a0a24905aed77e50
 K
 baekjoon/구슬탈출2.java,8/7/8731b08d166e90b3c8f022a80b5a598e732e62f8
-J
-baekjoon/바이러스.java,9/9/9954a4b1f517abc9a16dc4d38044eecd3734be80
 S
 #baekjoon/단지번호붙이기.java,e/2/e22cd6729a5179180f31015725a044ab0e897f86
 J
@@ -27,3 +25,29 @@ P
  baekjoon/주사위굴리기.java,e/e/eec77c559ec2c10c82c1d5ecf736693a15e2f5fa
 M
 baekjoon/로봇청소기.java,2/5/257901e7d7229b934ddeb2febf33132c95b2d6ba
+S
+#codetree/자율주행자동차.java,c/4/c494f259a4ff6a9c2d1246d47f77c124721ec17d
+Y
+)codetree/외주수익최대화하기.java,5/0/504f0edb2f1775e8656e5ebc622e6759be1ce557
+G
+baekjoon/연구소.java,9/f/9f6d953f4b6d7e865eab7524b6241b2d1b55f2d7
+D
+baekjoon/퇴사.java,3/6/36564043042767657fc44fc2ea8762881f8e44cb
+S
+#baekjoon/연산자끼워넣기.java,1/7/1785adc9f8a5d2fe2197d7ab171440ea5de67fdb
+J
+codetree/조삼모사.java,4/1/41291a0ee993260f734beeb94701832a2360e797
+S
+#codetree/연산자배치하기.java,e/7/e7b1f689ae3edf9d91f766f57c26831bce386ffb
+G
+baekjoon/토마토.java,2/3/2318f8f1c088dfbb1dfee977bbd9c8a95086724b
+J
+baekjoon/바이러스.java,9/9/9954a4b1f517abc9a16dc4d38044eecd3734be80
+M
+codetree/두개의사탕.java,c/f/cffb1638bac88db10aabf69d11f0db1dd374886a
+J
+baekjoon/촌수계산.java,e/7/e7102a85d09561c3dfaa8f0c67b5f835a64b09d9
+J
+codetree/나무박멸.java,1/8/189deb71db315865e11774ac98e22abb8badb6d2
+P
+ codetree/바이러스검사.java,1/9/1928c787fa50b8dbf7a5dfc9f28ca8b788688250
---
+++ src/baekjoon/숨바꼭질.java
@@ -0,0 +1,78 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 실버 1
+ * 백준 1697
+ */
+public class 숨바꼭질 {
+
+    static int N, K, ANS;
+    static boolean[] VISITED = new boolean[100001];
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        operate(new Pair(N, 0));
+
+        System.out.println(ANS);
+    }
+
+    public static void operate(Pair pair){
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(pair);
+        VISITED[pair.num] = true;
+
+        while(!queue.isEmpty()){
+            Pair now = queue.poll();
+            if(now.num == K){
+                ANS = now.count;
+                break;
+            }
+            if(isInbound(now.moveLeft()) && !VISITED[now.moveLeft()]){
+                VISITED[now.moveLeft()] = true;
+                queue.add(new Pair(now.moveLeft(), now.count + 1));
+            }
+            if(isInbound(now.moveRight()) && !VISITED[now.moveRight()]){
+                VISITED[now.moveRight()] = true;
+                queue.add(new Pair(now.moveRight(), now.count + 1));
+            }
+            if(isInbound(now.jump()) && !VISITED[now.jump()]){
+                VISITED[now.jump()] = true;
+                queue.add(new Pair(now.jump(), now.count + 1));
+            }
+        }
+    }
+
+    public static boolean isInbound(int n){
+        return n>=0 && n<=100000;
+    }
+}
+
+class Pair{
+
+    public Pair(int num, int count){
+        this.num = num;
+        this.count = count;
+    }
+    int num;
+    int count;
+
+    public int moveLeft(){
+        return num - 1;
+    }
+
+    public int moveRight(){
+        return num + 1;
+    }
+
+    public int jump(){
+        return num * 2;
+    }
+}
