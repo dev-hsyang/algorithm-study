f3ed2339c06ccc63f73bbc46fc3d124d8ad80b75
hyunseung yang <dev.hsyang@gmail.com>
2023-07-08 23:38:23 +0900

    add: 투포인터-수들의합5 (완)

---
+++ src/algorithm_coding/_03_3_투포인터/수들의합5.java
@@ -0,0 +1,36 @@
+package algorithm_coding._03_3_투포인터;
+
+import java.util.Scanner;
+
+/**
+ * 백준 2018
+ * 실버 5
+ */
+public class 수들의합5 {
+
+    static int N, ANS;
+    static int[] ARR;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        ARR = new int[N+1];
+        for(int i=1; i<N+1; i++)
+            ARR[i] = i;
+
+        operate();
+
+        System.out.println(ANS);
+    }
+    public static void operate(){
+        for(int i=1; i<N+1; i++){
+            int n = 0;
+            for(int j=i; j<N+1; j++){
+                n += j;
+                if(n == N)
+                    ANS += 1;
+                if(n > N)
+                    break;
+            }
+        }
+    }
+}
