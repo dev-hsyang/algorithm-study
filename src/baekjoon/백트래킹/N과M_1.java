55b9656a1d839141ece7145222d7764d0b6968b8
hyunseung yang <dev.hsyang@gmail.com>
2023-07-14 23:24:19 +0900

    fix: 백준-N과M_1 수정

--- src/baekjoon/백트래킹/N과M_1.java
+++ src/baekjoon/백트래킹/N과M_1.java
@@ -1,5 +1,6 @@
 package baekjoon.백트래킹;
 
+
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -13,9 +14,7 @@ public class N과M_1 {
 
 	static int N, M, PIVOT;
 	static int[] ANS;
-	static boolean[] VISITED;
-	static Stack<Integer> STACK = new Stack<>();
-	
+	static boolean[] VISITED;	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -34,10 +33,9 @@ public class N과M_1 {
 		}
 		
 		for(int i=1; i<N+1; i++) {
-			
 			if(!VISITED[i]) {
 				VISITED[i] = true;
-				ANS[depth] = i;
+				ANS[depth] = i; //** 핵심포인트.. depth 는 자릿수를 표현한다. 배열에서 depth 인덱스의 값은 해당 자릿수에 위치할 탐색된 값인 것이다.
 				operate(depth + 1);
 				VISITED[i] = false;
 			}
