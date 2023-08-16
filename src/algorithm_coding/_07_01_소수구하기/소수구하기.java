815fa7410397a1431d913aee2bfacb6a17f87fe8
hyunseung <dev.hsyang@gmail.com>
2023-08-16 23:57:27 +0900

    fix: 설명 주석 추가

--- src/algorithm_coding/_07_01_소수구하기/소수구하기.java
+++ src/algorithm_coding/_07_01_소수구하기/소수구하기.java
@@ -7,6 +7,14 @@ import java.util.Scanner;
  * 백준 1929
  * 실버 3
  */
+
+/**
+ * '에라토스테네스의 체' 를 활용한 소수 구하는 방식이다.
+ * 시간초과를 유발하지 않기 위한 중요한 두가지 포인트는,
+ *
+ * 1) 구하는 범위의 최대값의 제곱근까지만 탐색을 진행하고,
+ * 2) 이중 for 문을 구현할 때, 현재 i 의 배수일 경우만 for 문이 실행되도록 코드를 짜야 O(N^2) 이 아닌 O(Nlog(logN) 의 시간복잡도를 가지게 된다.
+ */
 public class 소수구하기 {
 
     static int N, M;
@@ -22,7 +30,7 @@ public class 소수구하기 {
             ARR[i] = i;
 
         for(int i=2; i<=Math.sqrt(N); i++)
-            for(int j=i+i; j<=N; j+=i)
+            for(int j=i+i; j<=N; j+=i) // j 가 i 의 배수인 경우로만 최적화 해두면 O(N^2) 가 되지 않는다.
                 if(ARR[j] % i == 0)
                     ARR[j] = 0;
 
