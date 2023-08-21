2a29ad0379868f5580ea9fd23b27b14055e00d8e
hyunseung <dev.hsyang@gmail.com>
2023-08-22 00:06:40 +0900

    fix: 익명 클래스 구현식 추가

--- src/algorithm_coding/_06_1_그리디알고리즘/회의실배정.java
+++ src/algorithm_coding/_06_1_그리디알고리즘/회의실배정.java
@@ -1,6 +1,7 @@
 package algorithm_coding._06_1_그리디알고리즘;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 /**
@@ -25,6 +26,15 @@ public class 회의실배정 {
                 return o1[0] - o2[0];
             return o1[1] - o2[1];
         });
+
+//        Arrays.sort(TIMELINE, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                if(o1[1] == o2[1])
+//                    return o1[0] - o2[0];
+//                return o1[1] - o2[1];
+//            }
+//        });
 
         int end = -1;
         for(int i=0; i<N; i++){
