d3a03fa20e096dd002af1962db9aa8d5b2daf4bb
hyunseung <dev.hsyang@gmail.com>
2023-09-02 01:41:37 +0900

    add: 백준 - N과M 3

---
+++ src/baekjoon/백트래킹/N과M_3.java
@@ -0,0 +1,46 @@
+package baekjoon.백트래킹;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+/**
+ * 백준 15651
+ * 실버 3
+ */
+public class N과M_3 {
+
+    static int N, M;
+    static int[] ARR;
+    static BufferedWriter BW;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BW = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = sc.nextInt();
+        M = sc.nextInt();
+        ARR = new int[N+1];
+        for(int i=1; i<N+1; i++)
+            ARR[i] = i;
+
+        dfs(0, new int[M]);
+
+        BW.flush();
+        BW.close();
+    }
+
+    public static void dfs(int depth, int[] arr) throws IOException {
+        if(depth == M){
+            for(int i : arr)
+                BW.append(i + " ");
+            BW.append("\n");
+            return;
+        }
+
+        for(int i=1; i<N+1; i++){
+            arr[depth] = i;
+            dfs(depth + 1, arr);
+        }
+    }
+}
