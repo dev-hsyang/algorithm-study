e4b2372ae7ea2d16f3149de39a1841b3ff74fa0a
hyunseung <dev.hsyang@gmail.com>
2023-09-02 00:58:22 +0900

    fix: code fix

--- src/baekjoon/백트래킹/N과M_2.java
+++ src/baekjoon/백트래킹/N과M_2.java
@@ -1,5 +1,7 @@
 package baekjoon.백트래킹;
 
+import java.util.Scanner;
+
 /**
  * 백준 156550
  * 실버 3
@@ -7,8 +9,31 @@ package baekjoon.백트래킹;
 public class N과M_2 {
 
     static int N, M;
+    static int[] ARR;
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        ARR = new int[N+1];
+        for(int i=1; i<N+1; i++)
+            ARR[i] = i;
 
+        dfs(1, 0, new int[M]);
+
+    }
+
+    public static void dfs(int num, int depth, int[] arr){
+        if(depth == M){
+            for(int i : arr)
+                System.out.print(i + " ");
+            System.out.println();
+            return;
+        }
+
+        for(int i=num; i<N+1; i++){
+            arr[depth] = i;
+            dfs(i + 1, depth + 1, arr);
+        }
     }
 }
