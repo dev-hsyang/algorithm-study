85272e44864da5b85c877ee9de7fc144428da92c
hyunseung <dev.hsyang@gmail.com>
2023-09-09 15:22:32 +0900

    add: 백준 - 스타트와 링크 (완)

---
+++ src/baekjoon/백트래킹/스타트와링크.java
@@ -0,0 +1,58 @@
+package baekjoon.백트래킹;
+
+import java.util.*;
+/**
+ * 백준 14889
+ * 실버 2
+ */
+
+public class 스타트와링크 {
+
+    static int N;
+    static int ANS = Integer.MAX_VALUE;
+    static int[][] BOARD;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N  = sc.nextInt();
+        BOARD = new int[N+1][N+1];
+        for(int i=1; i<N+1; i++)
+            for(int j=1; j<N+1; j++)
+                BOARD[i][j] = sc.nextInt();
+
+        dfs(0, 1, new int[N/2]);
+
+        System.out.println(ANS);
+    }
+
+    public static void dfs(int depth, int num, int[] arr){
+        if(depth == N/2){
+            ANS = Math.min(ANS, calculate(arr));
+            return;
+        }
+
+        for(int i=num; i<N+1; i++){
+            arr[depth] = i;
+            dfs(depth + 1, i + 1, arr);
+        }
+    }
+
+    public static int calculate(int[] arr){
+        int morningSum = 0;
+        int nightSum = 0;
+        boolean[] morning = new boolean[N+1];
+        for(int i : arr)
+            morning[i] = true;
+
+        for(int i=1; i<N+1; i++){
+            for(int j=1; j<N+1; j++){
+                if(morning[i] && morning[j]){
+                    morningSum += BOARD[i][j];
+                }else if(!morning[i] && !morning[j]){
+                    nightSum += BOARD[i][j];
+                }
+            }
+        }
+
+        return morningSum > nightSum ? morningSum - nightSum : nightSum - morningSum;
+    }
+}
