add3f6575209a8831ce1861ed509054441432bcf
hyunseung <dev.hsyang@gmail.com>
2023-09-12 01:00:04 +0900

    add: 백준 - 소문난칠공주 (완)

--- src/baekjoon/백트래킹/소문난칠공주.java
+++ src/baekjoon/백트래킹/소문난칠공주.java
@@ -3,6 +3,8 @@ package baekjoon.백트래킹;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
@@ -13,65 +15,95 @@ public class 소문난칠공주 {
 
     public static int ANS;
     public static int[] DX = {0, 1, 0, -1};
-    public static int[] DY = {1, 0, -1 ,0};
-    public static boolean[][] VISITED = new boolean[5][5];
+    public static int[] DY = {1, 0, -1, 0};
+    public static boolean[] VISITED = new boolean[25];
     public static String[][] CLASS = new String[5][5];
+    public static Seven[] ARR = new Seven[25];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        for(int i=0; i<5; i++){
+        int idx = 0;
+        for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
             String[] s = st.nextToken().split("");
-            for(int j=0; j<5; j++)
+            for (int j = 0; j < 5; j++){
                 CLASS[i][j] = s[j];
-        }
-
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
-                VISITED = new boolean[5][5];
-                //VISITED[i][j] = true;
-                backtrack(1, 0, i, j);
+                ARR[idx] = new Seven(i, j, CLASS[i][j]);
+                idx++;
             }
         }
+
+        findCombination(0, 0, 0);
 
         System.out.println(ANS);
     }
 
-    public static void backtrack(int depth, int cnt, int x, int y){
-        if(CLASS[x][y].equals("S"))
-            cnt += 1;
-
-        //VISITED[x][y] = true;
-
+    public static void findCombination(int depth, int idx, int cnt){
         if(depth == 7){
-            if(cnt >= 4){
-                printVisited();
+            if(cnt >= 4 && isAdjacent())
                 ANS++;
-            }
             return;
         }
-
-        for(int i=0; i<4; i++){
-            int nx = x + DX[i];
-            int ny = y + DY[i];
-            if(canGo(nx, ny) && !VISITED[nx][ny]){
-                VISITED[nx][ny] = true;
-                backtrack(depth + 1, cnt, nx, ny);
+        for(int i=idx; i<25; i++){
+            if(!VISITED[i]){
+                VISITED[i] = true;
+                if(ARR[i].side.equals("S"))
+                    findCombination(depth + 1, i + 1, cnt + 1);
+                else
+                    findCombination(depth + 1, i + 1, cnt);
+                VISITED[i] = false;
             }
         }
+    }
+
+    public static boolean isAdjacent(){
+        boolean[][] candiClass = new boolean[25][25];
+        int[] startPos = new int[2];
+
+        for(int i=0; i<25; i++)
+            if(VISITED[i]){
+                candiClass[ARR[i].x][ARR[i].y] = true;
+                startPos[0] = ARR[i].x;
+                startPos[1] = ARR[i].y;
+            }
+
+        return bfs(startPos, candiClass) == 7;
+    }
+
+    public static int bfs(int[] cord, boolean[][] candiClass){
+        int count = 1;
+        boolean[][] visited = new boolean[5][5];
+        Queue<int[]> queue = new LinkedList<>();
+        visited[cord[0]][cord[1]] = true;
+        queue.add(cord);
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
+            for(int i=0; i<4; i++){
+                int nx = now[0] + DX[i];
+                int ny = now[1] + DY[i];
+                if(canGo(nx, ny) && candiClass[nx][ny] && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    count++;
+                    queue.add(new int[] {nx, ny});
+                }
+            }
+        }
+        return count;
     }
 
     public static boolean canGo(int x, int y){
         return x>=0 && x<5 && y>=0 && y<5;
     }
+}
 
-    public static void printVisited(){
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++)
-                System.out.print(VISITED[i][j] + " ");
-            System.out.println();
-        }
-        System.out.println("");
+class Seven{
+
+    public Seven(int x, int y, String side){
+        this.x = x;
+        this.y = y;
+        this.side = side;
     }
+    String side;
+    int x, y;
 }
