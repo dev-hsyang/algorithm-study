4c57b62f56a00678e2c510bc22fa48119d4fa015
hyunseung <dev.hsyang@gmail.com>
2023-09-13 23:22:50 +0900

    add: 백준 - 계란으로계란치기 (완)

--- src/baekjoon/백트래킹/계란으로계란치기.java
+++ src/baekjoon/백트래킹/계란으로계란치기.java
@@ -11,39 +11,48 @@ public class 계란으로계란치기 {
     static int N, ANS;
     static int[] HP;
     static int[] WEIGHT;
-    static boolean[] VISITED;
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         HP = new int[N];
         WEIGHT = new int[N];
-        VISITED = new boolean[N];
         for(int i=0; i<N; i++){
             HP[i] = sc.nextInt();
             WEIGHT[i] = sc.nextInt();
         }
 
-        dfs(0);
+        dfs(0, 0);
 
         System.out.println(ANS);
     }
 
-    public static void dfs(int index){
+    public static void dfs(int index, int cracked){
         if(index == N)
             return;
 
+        if(HP[index] <= 0){
+            dfs(index + 1, cracked);
+            return;
+        }
+
         for(int i=0; i<N; i++){
-            if(HP[index] > 0 && HP[i] > 0 && i != index){
-                HP[index] -= WEIGHT[i];
+            if(i != index && HP[i] > 0){
+                int cnt = 0;
                 HP[i] -= WEIGHT[index];
-                ANS = Math.max(ANS, countCracked());
-                dfs(index + 1);
-                HP[index] += WEIGHT[i];
+                HP[index] -= WEIGHT[i];
+                if(HP[i] <= 0)
+                    cnt++;
+                if(HP[index] <= 0)
+                    cnt++;
+                ANS = Math.max(ANS, cracked + cnt);
+                dfs(index + 1, cracked + cnt);
                 HP[i] += WEIGHT[index];
+                HP[index] += WEIGHT[i];
             }
         }
     }
+
 
     public static int countCracked(){
         int cnt = 0;
