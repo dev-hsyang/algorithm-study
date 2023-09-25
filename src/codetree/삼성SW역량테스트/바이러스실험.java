112de21e0b022bbb79a7fc19a15fee026fe2ca9f
Hyunseung <didgsutmd1@ajou.ac.kr>
2023-09-25 23:42:10 +0900

    add: 코드트리 - 바이러스실험

---
+++ src/codetree/삼성SW역량테스트/바이러스실험.java
@@ -0,0 +1,15 @@
+package codetree.삼성SW역량테스트;
+
+/**
+ * 삼성 SW역량테스트 2018 하반기 오후 1번 문제
+ * @author didgs
+ *
+ */
+public class 바이러스실험 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
