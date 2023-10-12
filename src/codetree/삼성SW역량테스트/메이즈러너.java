49ba943bfd13af0bcef440b2b303d52ba6136285
Hyunseung <didgsutmd1@ajou.ac.kr>
2023-10-13 03:34:16 +0900

    fix: code fix

--- src/codetree/삼성SW역량테스트/메이즈러너.java
+++ src/codetree/삼성SW역량테스트/메이즈러너.java
@@ -84,13 +84,41 @@ public class 메이즈러너 {
 	
 	public static void mazeRotates() {
 		findMinSquare();
-		
-		int[] candiExit = new int[2];
-		int[] tempExit = new int[2];
+		rotateWall();
+		rotateExit();
+		rotateRunners();
+	}
+	
+	public static void findMinSquare() {
+		SQUARE_PAIR = new int[2];
+		MIN_LEN = 0;
+		for(int len = 2; len<=N; len++) { // 한변의 길이가 2인 정사각형부터 bruteforce하게 모든 정사각형 탐색
+			for(int i=1; i<=N-len+1; i++) {
+				for(int j=1; j<=N-len+1; j++) {
+					int x1 = i;
+					int y1 = j;
+					int x2 = x1 + len - 1;
+					int y2 = y1 + len - 1;
+					// 해당 정사각형 범위 내에 출구가 있는지 확인
+					if(!(EXIT[0] >= x1 && EXIT[0] <= x2 && EXIT[1] >= y1 && EXIT[1] <= y2))
+						continue;
+					for(int idx = 1; idx<=M; idx++) {
+						Runner2023 r = RUNNERS[idx];
+						if(!r.escape && r.x >= x1 && r.x <= x2 && r.y >= y1 && r.y <= y2) {
+							SQUARE_PAIR[0] = i;
+							SQUARE_PAIR[1] = j;
+							MIN_LEN = len;
+							return;
+						}
+					}	
+				}
+			}
+		}
+	}
+	
+	public static void rotateWall() {
 		int[][] candiMaze = new int[MIN_LEN][MIN_LEN];
 		int[][] tempMaze = new int[MIN_LEN][MIN_LEN];
-
-		// 미로 벽 회전
 		for(int i=0; i<MIN_LEN; i++)
 			for(int j=0; j<MIN_LEN; j++)
 				tempMaze[i][j] = MAZE[SQUARE_PAIR[0] + i][SQUARE_PAIR[1] + j];
@@ -104,16 +132,20 @@ public class 메이즈러너 {
 					num = 0;
 				MAZE[SQUARE_PAIR[0] + i][SQUARE_PAIR[1] + j] = num;
 			}
-		// 출구 회전
-		if(EXIT[0] >= SQUARE_PAIR[0] && EXIT[0] < SQUARE_PAIR[0] + MIN_LEN && EXIT[1] >= SQUARE_PAIR[1] && EXIT[1] < SQUARE_PAIR[1] + MIN_LEN) {			
-				tempExit[0] = EXIT[0] - SQUARE_PAIR[0];
-				tempExit[1] = EXIT[1] - SQUARE_PAIR[1];
-				candiExit[0] = tempExit[1];
-				candiExit[1] = MIN_LEN - tempExit[0] - 1;
-				EXIT[0] = candiExit[0] + SQUARE_PAIR[0];
-				EXIT[1] = candiExit[1] + SQUARE_PAIR[1];
-		}
-		// 사용자 회전
+	}
+	
+	public static void rotateExit() {
+		int[] candiExit = new int[2];
+		int[] tempExit = new int[2];
+		tempExit[0] = EXIT[0] - SQUARE_PAIR[0];
+		tempExit[1] = EXIT[1] - SQUARE_PAIR[1];
+		candiExit[0] = tempExit[1];
+		candiExit[1] = MIN_LEN - tempExit[0] - 1;
+		EXIT[0] = candiExit[0] + SQUARE_PAIR[0];
+		EXIT[1] = candiExit[1] + SQUARE_PAIR[1];
+	}
+	
+	public static void rotateRunners() {
 		for(int i=1; i<=M; i++) {
 			Runner2023 r = RUNNERS[i];
 			if(r.x >= SQUARE_PAIR[0] && r.x < SQUARE_PAIR[0] + MIN_LEN && r.y >= SQUARE_PAIR[1] && r.y < SQUARE_PAIR[1] + MIN_LEN) {
@@ -123,35 +155,6 @@ public class 메이즈러너 {
 				int ny = MIN_LEN - tx - 1;
 				r.x = nx + SQUARE_PAIR[0];
 				r.y = ny + SQUARE_PAIR[1];
-			}
-		}
-	}
-	
-	public static void findMinSquare() {
-		SQUARE_PAIR = new int[2];
-		MIN_LEN = 0;
-		for(int len = 2; len<=N; len++) { // 한변의 길이가 2인 정사각형부터 bruteforce하게 모든 정사각형 탐색
-			for(int i=1; i<=N-len+1; i++) {
-				for(int j=1; j<=N-len+1; j++) {
-					int x1 = i;
-					int y1 = j;
-					int x2 = x1 + len - 1;
-					int y2 = y1 + len - 1;
-					
-					// 해당 정사각형 범위 내에 출구가 있는지 확인
-					if(!(EXIT[0] >= x1 && EXIT[0] <= x2 && EXIT[1] >= y1 && EXIT[1] <= y2))
-						continue;
-					
-					for(int idx = 1; idx<=M; idx++) {
-						Runner2023 r = RUNNERS[idx];
-						if(!r.escape && r.x >= x1 && r.x <= x2 && r.y >= y1 && r.y <= y2) {
-							SQUARE_PAIR[0] = i;
-							SQUARE_PAIR[1] = j;
-							MIN_LEN = len;
-							return;
-						}
-					}	
-				}
 			}
 		}
 	}
